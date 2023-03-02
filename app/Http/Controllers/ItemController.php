7406da12c56a82c6fdd5647e0a9b2172f7feb0e8
阪田里実 <>
2023-03-02 15:13:16 +0900

    商品一覧、登録の作成

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -5,22 +5,32 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreItemRequest;
 use App\Http\Requests\UpdateItemRequest;
 use App\Models\Item;
+use Inertia\Inertia;
 
 class ItemController extends Controller
 {
     public function index()
     {
-        //
+        return Inertia::render('Items/Index', [
+            'items' => Item::select('id', 'name', 'price', 'is_selling')
+            ->get()
+        ]);
     }
 
     public function create()
     {
-        //
+        return Inertia::render('Items/Create');
     }
 
     public function store(StoreItemRequest $request)
     {
-        //
+        Item::create([
+            'name' => $request->name,
+            'memo' => $request->memo,
+            'price' => $request->price,
+        ]);
+
+        return to_route('items.index');
     }
 
     public function show(Item $item)
